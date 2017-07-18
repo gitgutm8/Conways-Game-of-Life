8db14780819aa5f2bc0ad38683a7f55ecf1e497b
gitgutm8 <clemensring@gmail.com>
2017-07-18 16:50:01 +0200

    - Added PyGame demo and improved cmd demo

--- demo.py
+++ demo.py
@@ -1,4 +1,22 @@
 import gol
+import pggol as pgol
 
-init = gol.from_file('examples.json', 'period15')
-gol.sim(init, 20, 20)
+
+def demo_cmd():
+    lines, cols = 20, 20
+    init = gol.from_file('examples.json', 'period15', lines, cols, middle=True)
+    gol.sim(init, 20, 20, 30)
+    init = gol.from_file('examples.json', 'pulsar', lines, cols, middle=True)
+    gol.sim(init, 20, 20, 30)
+
+
+def demo_pygame():
+    pgol.pg.init()
+    lines, cols = 100, 100
+    demo = pgol.PgGol.from_file('examples.json', '54turns', lines, cols, middle=True)
+    demo.initialising = False
+    demo.run()
+
+if __name__ == '__main__':
+    inp = input('What do you want to see?\n1> demo in commandline\n2> demo in PyGame\n')
+    {'1': demo_cmd, '2': demo_pygame}[inp]()
